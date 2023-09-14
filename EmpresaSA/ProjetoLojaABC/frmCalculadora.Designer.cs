a4b8c6fd353f5a98cd2ba89fe6e47eb310229a07
jumalisilva <juliamalisilva@gmail.com>
2023-09-14 17:11:32 -0300

    UC12

---
+++ EmpresaSA/ProjetoLojaABC/frmCalculadora.Designer.cs
@@ -0,0 +1,253 @@
+﻿
+namespace ProjetoLojaABC
+{
+    partial class frmCalculadora
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmCalculadora));
+            this.gpbOperacoes = new System.Windows.Forms.GroupBox();
+            this.rdbDivisao = new System.Windows.Forms.RadioButton();
+            this.rdbMultiplicacao = new System.Windows.Forms.RadioButton();
+            this.rdbSubtracao = new System.Windows.Forms.RadioButton();
+            this.rdbAdicao = new System.Windows.Forms.RadioButton();
+            this.lblVariavel1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lblVariavel2 = new System.Windows.Forms.Label();
+            this.lblResultado = new System.Windows.Forms.Label();
+            this.lblTotal = new System.Windows.Forms.Label();
+            this.txtVariavel1 = new System.Windows.Forms.TextBox();
+            this.txtVariavel2 = new System.Windows.Forms.TextBox();
+            this.btnCalcular = new System.Windows.Forms.Button();
+            this.btnLimpar = new System.Windows.Forms.Button();
+            this.btnSair = new System.Windows.Forms.Button();
+            this.gpbOperacoes.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // gpbOperacoes
+            // 
+            this.gpbOperacoes.Controls.Add(this.rdbDivisao);
+            this.gpbOperacoes.Controls.Add(this.rdbMultiplicacao);
+            this.gpbOperacoes.Controls.Add(this.rdbSubtracao);
+            this.gpbOperacoes.Controls.Add(this.rdbAdicao);
+            this.gpbOperacoes.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.gpbOperacoes.Location = new System.Drawing.Point(323, 139);
+            this.gpbOperacoes.Name = "gpbOperacoes";
+            this.gpbOperacoes.Size = new System.Drawing.Size(215, 180);
+            this.gpbOperacoes.TabIndex = 2;
+            this.gpbOperacoes.TabStop = false;
+            this.gpbOperacoes.Text = "Operações";
+            // 
+            // rdbDivisao
+            // 
+            this.rdbDivisao.AutoSize = true;
+            this.rdbDivisao.Location = new System.Drawing.Point(49, 138);
+            this.rdbDivisao.Name = "rdbDivisao";
+            this.rdbDivisao.Size = new System.Drawing.Size(96, 24);
+            this.rdbDivisao.TabIndex = 6;
+            this.rdbDivisao.TabStop = true;
+            this.rdbDivisao.Text = "Divisão (/)";
+            this.rdbDivisao.UseVisualStyleBackColor = true;
+            // 
+            // rdbMultiplicacao
+            // 
+            this.rdbMultiplicacao.AutoSize = true;
+            this.rdbMultiplicacao.Location = new System.Drawing.Point(49, 108);
+            this.rdbMultiplicacao.Name = "rdbMultiplicacao";
+            this.rdbMultiplicacao.Size = new System.Drawing.Size(138, 24);
+            this.rdbMultiplicacao.TabIndex = 5;
+            this.rdbMultiplicacao.TabStop = true;
+            this.rdbMultiplicacao.Text = "Multiplicação (*)";
+            this.rdbMultiplicacao.UseVisualStyleBackColor = true;
+            // 
+            // rdbSubtracao
+            // 
+            this.rdbSubtracao.AutoSize = true;
+            this.rdbSubtracao.Location = new System.Drawing.Point(49, 78);
+            this.rdbSubtracao.Name = "rdbSubtracao";
+            this.rdbSubtracao.Size = new System.Drawing.Size(120, 24);
+            this.rdbSubtracao.TabIndex = 4;
+            this.rdbSubtracao.TabStop = true;
+            this.rdbSubtracao.Text = "Subtração (-)";
+            this.rdbSubtracao.UseVisualStyleBackColor = true;
+            // 
+            // rdbAdicao
+            // 
+            this.rdbAdicao.AutoSize = true;
+            this.rdbAdicao.Location = new System.Drawing.Point(49, 48);
+            this.rdbAdicao.Name = "rdbAdicao";
+            this.rdbAdicao.Size = new System.Drawing.Size(99, 24);
+            this.rdbAdicao.TabIndex = 3;
+            this.rdbAdicao.TabStop = true;
+            this.rdbAdicao.Text = "Adição (+)";
+            this.rdbAdicao.UseVisualStyleBackColor = true;
+            // 
+            // lblVariavel1
+            // 
+            this.lblVariavel1.AutoSize = true;
+            this.lblVariavel1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblVariavel1.Location = new System.Drawing.Point(62, 83);
+            this.lblVariavel1.Name = "lblVariavel1";
+            this.lblVariavel1.Size = new System.Drawing.Size(78, 20);
+            this.lblVariavel1.TabIndex = 1;
+            this.lblVariavel1.Text = "Variável 1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(63, 108);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(0, 13);
+            this.label2.TabIndex = 2;
+            // 
+            // lblVariavel2
+            // 
+            this.lblVariavel2.AutoSize = true;
+            this.lblVariavel2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblVariavel2.Location = new System.Drawing.Point(62, 139);
+            this.lblVariavel2.Name = "lblVariavel2";
+            this.lblVariavel2.Size = new System.Drawing.Size(78, 20);
+            this.lblVariavel2.TabIndex = 3;
+            this.lblVariavel2.Text = "Variável 2";
+            // 
+            // lblResultado
+            // 
+            this.lblResultado.AutoSize = true;
+            this.lblResultado.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblResultado.Location = new System.Drawing.Point(619, 83);
+            this.lblResultado.Name = "lblResultado";
+            this.lblResultado.Size = new System.Drawing.Size(82, 20);
+            this.lblResultado.TabIndex = 8;
+            this.lblResultado.Text = "Resultado";
+            // 
+            // lblTotal
+            // 
+            this.lblTotal.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.lblTotal.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTotal.Location = new System.Drawing.Point(623, 124);
+            this.lblTotal.Name = "lblTotal";
+            this.lblTotal.Size = new System.Drawing.Size(178, 31);
+            this.lblTotal.TabIndex = 9;
+            this.lblTotal.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // txtVariavel1
+            // 
+            this.txtVariavel1.Location = new System.Drawing.Point(165, 83);
+            this.txtVariavel1.Name = "txtVariavel1";
+            this.txtVariavel1.Size = new System.Drawing.Size(106, 20);
+            this.txtVariavel1.TabIndex = 0;
+            // 
+            // txtVariavel2
+            // 
+            this.txtVariavel2.Location = new System.Drawing.Point(165, 139);
+            this.txtVariavel2.Name = "txtVariavel2";
+            this.txtVariavel2.Size = new System.Drawing.Size(106, 20);
+            this.txtVariavel2.TabIndex = 1;
+            // 
+            // btnCalcular
+            // 
+            this.btnCalcular.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.btnCalcular.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCalcular.Location = new System.Drawing.Point(623, 183);
+            this.btnCalcular.Name = "btnCalcular";
+            this.btnCalcular.Size = new System.Drawing.Size(178, 42);
+            this.btnCalcular.TabIndex = 7;
+            this.btnCalcular.Text = "Calcular";
+            this.btnCalcular.UseVisualStyleBackColor = false;
+            this.btnCalcular.Click += new System.EventHandler(this.btnCalcular_Click);
+            // 
+            // btnLimpar
+            // 
+            this.btnLimpar.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.btnLimpar.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnLimpar.Location = new System.Drawing.Point(623, 252);
+            this.btnLimpar.Name = "btnLimpar";
+            this.btnLimpar.Size = new System.Drawing.Size(178, 42);
+            this.btnLimpar.TabIndex = 8;
+            this.btnLimpar.Text = "Limpar";
+            this.btnLimpar.UseVisualStyleBackColor = false;
+            // 
+            // btnSair
+            // 
+            this.btnSair.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.btnSair.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSair.Location = new System.Drawing.Point(623, 321);
+            this.btnSair.Name = "btnSair";
+            this.btnSair.Size = new System.Drawing.Size(178, 42);
+            this.btnSair.TabIndex = 9;
+            this.btnSair.Text = "Sair";
+            this.btnSair.UseVisualStyleBackColor = false;
+            this.btnSair.Click += new System.EventHandler(this.btnSair_Click);
+            // 
+            // frmCalculadora
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(905, 418);
+            this.Controls.Add(this.btnSair);
+            this.Controls.Add(this.btnLimpar);
+            this.Controls.Add(this.btnCalcular);
+            this.Controls.Add(this.txtVariavel2);
+            this.Controls.Add(this.txtVariavel1);
+            this.Controls.Add(this.lblTotal);
+            this.Controls.Add(this.lblResultado);
+            this.Controls.Add(this.lblVariavel2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.lblVariavel1);
+            this.Controls.Add(this.gpbOperacoes);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "frmCalculadora";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Calculadora";
+            this.gpbOperacoes.ResumeLayout(false);
+            this.gpbOperacoes.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox gpbOperacoes;
+        private System.Windows.Forms.RadioButton rdbDivisao;
+        private System.Windows.Forms.RadioButton rdbMultiplicacao;
+        private System.Windows.Forms.RadioButton rdbSubtracao;
+        private System.Windows.Forms.RadioButton rdbAdicao;
+        private System.Windows.Forms.Label lblVariavel1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label lblVariavel2;
+        private System.Windows.Forms.Label lblResultado;
+        private System.Windows.Forms.Label lblTotal;
+        private System.Windows.Forms.TextBox txtVariavel1;
+        private System.Windows.Forms.TextBox txtVariavel2;
+        private System.Windows.Forms.Button btnCalcular;
+        private System.Windows.Forms.Button btnLimpar;
+        private System.Windows.Forms.Button btnSair;
+    }
+}
